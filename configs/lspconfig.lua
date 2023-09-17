11689fb1920e495ce5469f60632378b3535e8ed2
wrnlb666 <wrnlb666@gmail.com>
2023-09-17 12:10:53 -0400

    omnisharp

--- configs/lspconfig.lua
+++ configs/lspconfig.lua
@@ -4,7 +4,7 @@ local capabilities = require("plugins.configs.lspconfig").capabilities
 local lspconfig = require "lspconfig"
 
 -- if you just want default config for the servers then put them in a table
-local servers = { "html", "cssls", "tsserver", "gopls", "pyright", "rust_analyzer", "jdtls", "omnisharp" }
+local servers = { "html", "cssls", "tsserver", "gopls", "pyright", "rust_analyzer", "jdtls" }
 
 for _, lsp in ipairs(servers) do
     lspconfig[lsp].setup {
@@ -15,11 +15,24 @@ end
 
 -- 
 -- lspconfig.pyright.setup { blabla}
+
+-- clangd
 lspconfig.clangd.setup {
     on_attach = on_attach,
     capabilities = capabilities,
     cmd = {
-        "clangd",
+        "/usr/bin/clangd",
         "--offset-encoding=utf-16",
     },
+}
+
+-- omnisharp
+lspconfig.omnisharp.setup {
+    on_attach = on_attach,
+    capabilities = capabilities,
+    cmd = {
+        "/usr/bin/omnisharp",
+    },
+    enable_roslyn_analyzers = true,
+    organize_imports_on_format = true,
 }
