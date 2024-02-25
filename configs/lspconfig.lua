19ad69f51512a737beed32358ec074c5acc2a6a2
wrnlb666 <wrnlb666@gmail.com>
2024-02-25 18:15:07 +0800

    kotlin ls incorrect spell

--- configs/lspconfig.lua
+++ configs/lspconfig.lua
@@ -4,7 +4,7 @@ local capabilities = require("plugins.configs.lspconfig").capabilities
 local lspconfig = require "lspconfig"
 
 -- if you just want default config for the servers then put them in a table
-local servers = { "html", "cssls", "tsserver", "gopls", "bufls", "pyright", "zls", "rust_analyzer", "jdtls", "kotlin-language-server" }
+local servers = { "html", "cssls", "tsserver", "gopls", "bufls", "pyright", "zls", "rust_analyzer", "jdtls", "kotlin_language_server" }
 
 for _, lsp in ipairs(servers) do
     lspconfig[lsp].setup {
