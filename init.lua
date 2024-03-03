b37ac66bb1b66e86b38162aba7a23a1b36305297
wrnlb666 <wrnlb666@gmail.com>
2024-03-04 01:46:14 +0800

    better key binding for treesitter folds

--- init.lua
+++ init.lua
@@ -34,6 +34,19 @@ end
 vim.opt.foldmethod = "expr"
 vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
 
+-- Swapping 'za' with 'zA'
+vim.api.nvim_set_keymap('n', 'za', 'zA', {noremap = true})
+vim.api.nvim_set_keymap('n', 'zA', 'za', {noremap = true})
+
+-- Swapping 'zr' with 'zR'
+vim.api.nvim_set_keymap('n', 'zr', 'zR', {noremap = true})
+vim.api.nvim_set_keymap('n', 'zR', 'zr', {noremap = true})
+
+-- Swapping 'zm' with 'zM'
+vim.api.nvim_set_keymap('n', 'zm', 'zM', {noremap = true})
+vim.api.nvim_set_keymap('n', 'zM', 'zm', {noremap = true})
+
+
 -- git branches
 vim.api.nvim_set_keymap('n', '<leader>co', '<cmd>Telescope git_branches<CR>', {noremap = true, silent = true})
 vim.api.nvim_set_keymap('v', 'D', '"_d', {noremap = true})
